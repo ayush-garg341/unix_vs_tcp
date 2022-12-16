6140e712955b1fbc1792d596df8f3d7287a566e9
Ayush Garg <gargayush341@gmail.com>
2022-12-16 11:16:44 +0530

    code fixed for strlen and sizeof and difference noted

--- c_code/tcp/client.c
+++ c_code/tcp/client.c
@@ -1,65 +1,70 @@
-#include <stdio.h>
-#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <sys/un.h>
+#include <stdio.h>
 #include <unistd.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <arpa/inet.h>
 
-int main(void)
-{
-    int socket_desc;
-    struct sockaddr_in server_addr;
-    char server_message[100], client_message[100];
-    
-    
-    // Create socket:
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if(socket_desc < 0){
-        printf("Unable to create socket\n");
-        return -1;
-    }
-    
-    printf("Socket created successfully\n");
-    
-    // Set port and IP the same as server-side:
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    
-    // Send connection request to server:
-    if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
-        printf("Unable to connect\n");
-        return -1;
-    }
-    printf("Connected with server successfully\n");
-    
-    // Get input from the user:
-    while(1){
-        // Clean buffers:
-        memset(server_message,'\0',sizeof(server_message));
-        memset(client_message,'\0',sizeof(client_message));
-        printf("Enter message: ");
-        gets(client_message);
-        
-        // Send the message to server:
-        if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
-            printf("Unable to send message\n");
-            return -1;
+int main(int argc, char *argv[]){
+	int socket_desc;
+	struct sockaddr_in server_addr;
+	char client_msg[2000], server_msg[2000];
+
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_desc < 0){
+		perror("Can't open socket\n");
+		exit(1);
+	}
+	printf("Socket created successfully\n");
+
+
+	// Set port and IP
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(2000);
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+		perror("Can't connect to server\n");
+		exit(1);
+	}
+	printf("Connected with server successfully\n");
+	// Get input from the user:
+  while(1){
+      // Clean the buffers
+      memset(server_msg, '\0', sizeof(server_msg));
+      memset(client_msg, '\0', sizeof(client_msg));
+
+      printf("Enter message : ");
+      gets(client_msg);
+
+      // Send the message to server
+      if(send(socket_desc, client_msg, strlen(client_msg), 0) < 0){
+          printf("Can't send msg to server\n");
+          return -1;
         }
-        
-        // Receive the server's response:
-        if(recv(socket_desc, server_message, sizeof(server_message), 0) < 0){
-            printf("Error while receiving server's msg\n");
-            return -1;
+
+      // Recv the msg from server
+      if(recv(socket_desc, server_msg, sizeof(server_msg), 0) < 0){
+          printf("Can't receive the msg from server\n");
+          return -1;
         }
-        
-        printf("Server's response: %s\n",server_message);
-        
-    }
-    // Close the socket:
-    close(socket_desc);
-    
-    return 0;
+      printf("Len of message returned by server : %ld\n", strlen(server_msg));
+      printf("Size of message returned by server : %ld\n", sizeof(server_msg));
+      printf("Server's response: %s\n",server_msg);
+  }
+	// Close the socket
+	close(socket_desc);
+	return 0;
 }
 
+
+/*
+ * Difference between sizeof and strlen
+ * strlen method is used to find the length of null terminated string.
+ * sizeof method is used to find the actual size of data.
+ * In the above example if in recv call we change sizeof to strlen, we will get nothing in server_msg. The reason being strlen(server_msg) is 0 as it's null terminated but sizeof(server_msg) is 
+ * 2000 as it contains 2000 bytes of data. So in recv we will read 0 bytes in buffer and hence empty server_msg.
+ */
--- c_code/tcp/server.c
+++ c_code/tcp/server.c
@@ -1,79 +1,80 @@
-#include <stdio.h>
-#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <sys/un.h>
+#include <stdio.h>
 #include <unistd.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <arpa/inet.h>
 
-int main(void)
-{
-    int socket_desc, client_sock, client_size;
-    struct sockaddr_in server_addr, client_addr;
-    char server_message[100], client_message[100];
-    
-    
-    // Create socket:
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if(socket_desc < 0){
-        printf("Error while creating socket\n");
-        return -1;
-    }
-    printf("Socket created successfully\n");
-    
-    // Set port and IP:
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2000);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    
-    // Bind to the set port and IP:
-    if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr))<0){
-        printf("Couldn't bind to the port\n");
-        return -1;
-    }
-    printf("Done with binding\n");
-    
-    // Listen for clients:
-    if(listen(socket_desc, 1) < 0){
-        printf("Error while listening\n");
-        return -1;
-    }
-    printf("\nListening for incoming connections.....\n");
-    
-    // Accept an incoming connection:
-    client_size = sizeof(client_addr);
-    client_sock = accept(socket_desc, (struct sockaddr*)&client_addr, &client_size);
-    
-    if (client_sock < 0){
-        printf("Can't accept\n");
-        return -1;
-    }
-    printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-    
-    while(1){
+int main(int argc, char *argv[]){
+
+	int socket_desc, client_sock, client_size;
+	struct sockaddr_in server_addr, client_addr;
+	char server_msg[2000], client_msg[2000];
+
+
+	// Create socket
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_desc < 0){
+		perror("Can;t open socket");
+		exit(1);
+	}
+	printf("Socket created successfully\n");
+
+	// Set port and IP
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(2000);
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	// Bind the socket descriptor to IP and port
+	if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+		perror("Couldn't bind to the port\n");
+		exit(1);
+	}
+	printf("Successfully bind\n");
+
+	// Listen for clients
+	if(listen(socket_desc, 1) < 0){
+		perror("Error while listening\n");
+		exit(1);
+	}
+	printf("\nListening for incoming connections.....\n");
+
+	// Accept an incoming connection
+	client_size = sizeof(client_addr);
+	client_sock = accept(socket_desc, (struct sockaddr*)&client_addr, &client_size);
+	if (client_sock < 0){
+		perror("Can't accept \n");
+		exit(1);
+	}
+	printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+  while(1){
         // Receive client's message:
         // Clean buffers:
-        memset(server_message, '\0', sizeof(server_message));
-        memset(client_message, '\0', sizeof(client_message));
-        if (recv(client_sock, client_message, sizeof(client_message), 0) < 0){
+        memset(server_msg, '\0', sizeof(server_msg));
+        memset(client_msg, '\0', sizeof(client_msg));
+        if (recv(client_sock, client_msg, sizeof(client_msg), 0) < 0){
             printf("Couldn't receive\n");
             return -1;
         }
-        printf("Msg from client: %s\n", client_message);
-        
+        printf("Len of message returned by client : %ld\n", strlen(client_msg));
+        printf("Size of message returned by client : %ld\n", sizeof(client_msg));
+        printf("Msg from client: %s\n", client_msg);
+
         // Respond to client:
-        strcpy(server_message, "Ack by server");
-        
-        if (send(client_sock, server_message, strlen(server_message), 0) < 0){
+        strcpy(server_msg, "Ack by server");
+
+        if (send(client_sock, server_msg, strlen(server_msg), 0) < 0){
             printf("Can't send\n");
             return -1;
         }
-    
-    }
-    // Closing the socket:
-    close(client_sock);
-    close(socket_desc);
-    
-    return 0;
-}
 
+    }
+
+	// Close the sockets
+	close(client_sock);
+	close(socket_desc);
+	return 0;
+}
